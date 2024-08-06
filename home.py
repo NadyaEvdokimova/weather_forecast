e02a131f68755ee3e9f653290c0c0154d532cdb4
valko <valkoista49@gmail.com>
2024-08-06 17:13:44 +0200

    Get weather data from Weather API

--- home.py
+++ home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
@@ -7,15 +8,7 @@ days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the num
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-
-def get_data(days):
-    all_dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-    all_temperatures = [10, 11, 15]
-    all_temperatures = [days * i for i in all_temperatures]
-    return all_dates, all_temperatures
-
-
-dates, temperatures = get_data(days)
+data = get_data(place, days, option)
 
 figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperatures (C)"})
 st.plotly_chart(figure)
