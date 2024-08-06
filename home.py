d34c6fc2ca993d28bb7279beeb0406116db5943c
valko <valkoista49@gmail.com>
2024-08-06 17:34:37 +0200

    Fix issue with not existing city

--- home.py
+++ home.py
@@ -10,23 +10,26 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
-    # Get the temperature/sky data
-    data = get_data(place, days)
+    try:
+        # Get the temperature/sky data
+        data = get_data(place, days)
 
-    if option == "Temperature":
-        temperatures = [part["main"]["temp"] / 10 for part in data]
-        dates = [part["dt_txt"] for part in data]
-        # Create a temperature plot
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperatures (C)"})
-        st.plotly_chart(figure)
-    elif option == "Sky":
-        sky_conditions = [part["weather"][0]["main"] for part in data]
-        images = {
-            "Clear": "images/clear.png",
-            "Clouds": "images/cloud.png",
-            "Rain": "images/rain.png",
-            "Snow": "images/snow.png"
-        }
-        image_paths = [images[condition] for condition in sky_conditions]
-        st.image(image_paths, width=115)
+        if option == "Temperature":
+            temperatures = [part["main"]["temp"] / 10 for part in data]
+            dates = [part["dt_txt"] for part in data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperatures (C)"})
+            st.plotly_chart(figure)
+        elif option == "Sky":
+            sky_conditions = [part["weather"][0]["main"] for part in data]
+            images = {
+                "Clear": "images/clear.png",
+                "Clouds": "images/cloud.png",
+                "Rain": "images/rain.png",
+                "Snow": "images/snow.png"
+            }
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.warning("You entered not existing city, please try again.")
 
